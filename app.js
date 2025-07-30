a7aa8ac0318433e2aa61798a892388fa95cc1ed2
Lautaro Lamaita <lautarolamaita@gmail.com>
2025-07-30 13:38:15 -0300

    Sorter-amigos-terminado

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
-let nombres = [];
+let amigos = [];
 
 function agregarAmigo() {
   let nombreIngresado = document.getElementById("amigo").value;
@@ -7,19 +7,32 @@ function agregarAmigo() {
   if (nombreIngresado == "") {
     alert("Por favor, inserte un nombre.");
   } else {
-    nombres.push(nombreIngresado);
-    actualizarAmigo(nombres);
+    amigos.push(nombreIngresado);
+    actualizarAmigo(amigos);
   }
   return (document.querySelector("#amigo").value = "");
 }
 
-function actualizarAmigo(nombres) {
+function actualizarAmigo(amigos) {
   let lista = document.getElementById("listaAmigos");
   lista.innerHTML = "";
 
-  for (let i = 0; i < nombres.length; i++) {
+  for (let i = 0; i < amigos.length; i++) {
     let item = document.createElement("li");
-    item.textContent = nombres[i];
+    item.textContent = amigos[i];
     lista.appendChild(item);
+  }
+}
+
+function sortearAmigo() {
+  if (amigos.length < 2) {
+    alert("Para poder sortear se necesita al menos 2 nombres!");
+    return;
+  } else {
+    let indiceAleatorio = Math.floor(Math.random() * amigos.length);
+    let amigoSecreto = amigos[indiceAleatorio];
+
+    let resultadoAmigo = document.getElementById("resultado");
+    resultadoAmigo.innerHTML = `<li>Tu amigo secreto es: ${amigoSecreto}</li>`;
   }
 }
