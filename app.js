a9172cb6baa45d24f155421416b75303216f6f4c
Lautaro Lamaita <lautarolamaita@gmail.com>
2025-07-30 12:25:38 -0300

    Funcion-agregar-amigo-realizada

--- app.js
+++ app.js
@@ -1,2 +1,14 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 let nombres = [];
+
+function agregarAmigo() {
+  let nombreIngresado = document.getElementById("amigo").value;
+
+  if (nombreIngresado == "") {
+    alert("Por favor, inserte un nombre.");
+  } else {
+    nombres.push(nombreIngresado);
+  }
+  console.log(nombres);
+  return (document.querySelector("#amigo").value = "");
+}
